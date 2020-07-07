3f560ea5a043d3f9ba064a3be813ff5f1d890ce6
melZula <zula0747@gmail.com>
2020-07-07 20:11:42 +0300

    created trigger

--- migrations/20200704180838_create_data.up.sql
+++ migrations/20200704180838_create_data.up.sql
@@ -4,3 +4,18 @@ CREATE TABLE securebin (
     encrypted_password varchar not null,
     lifetime timestamp without time zone DEFAULT (CURRENT_TIMESTAMP + '1 day'::interval) NOT NULL
 );
+
+CREATE FUNCTION public.del_expired()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+     NOT LEAKPROOF
+AS $BODY$BEGIN
+DELETE FROM securebin WHERE lifetime < CURRENT_TIMESTAMP;
+RETURN NULL;
+END$BODY$;
+
+CREATE TRIGGER del_exp
+    AFTER INSERT
+    ON public.securebin
+    FOR EACH STATEMENT
+    EXECUTE PROCEDURE public.del_expired();
